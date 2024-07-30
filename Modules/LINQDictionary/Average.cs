adc586fa8c21288cba7e0c19159fb49897109ce8
Adam Grgić <adam@grgicconsulting.com>
2024-07-29 23:29:38 -0600

    Linq Dictionary AsEnumerable , Average , Cast

---
+++ Modules/LINQDictionary/Average.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SandboxModule.Modules.LINQDictionary
+{
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.average?view=net-8.0
+
+    // get average of a sequence of values
+    // can be used for many different data 
+
+    [Route("[controller]/[action]")]
+    public class Average : Controller
+    {
+        public IActionResult V1()
+        {
+            int[] numbers = { 25, 69, 500, 746 };
+
+            double? average = numbers.Average();
+
+            return Ok();
+        }
+    }
+}
