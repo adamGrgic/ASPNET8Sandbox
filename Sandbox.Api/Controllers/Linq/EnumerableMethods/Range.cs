6fc0981f6cd5c7f448fcae3d5ee20de7307663f2
adamGrgic <argrgic@gmail.com>
2024-08-19 23:38:13 -0600

    Enumerable.Range

---
+++ Sandbox.Api/Controllers/Linq/EnumerableMethods/Range.cs
@@ -0,0 +1,26 @@
+﻿namespace Sandbox.Api.Controllers.Linq.EnumerableMethods
+{
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.range?view=net-8.0
+    public class Range : ControllerBase
+    {
+        public Range()
+        {
+
+        }
+
+        public IActionResult V1()
+        {
+            // Generate a sequence of integers from 1 to 10
+            // and then select their squares.
+            IEnumerable<int> squares = Enumerable.Range(1, 10).Select(x => x * x);
+
+            foreach (int num in squares)
+            {
+                Console.WriteLine(num);
+            }
+
+            return Ok();
+        }
+
+    }
+}
