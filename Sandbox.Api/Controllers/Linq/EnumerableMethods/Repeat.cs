a14f8076964d0c509d5846d57a190c705e648829
adamGrgic <argrgic@gmail.com>
2024-08-20 21:34:36 -0600

    Enumerable.Repeat

---
+++ Sandbox.Api/Controllers/Linq/EnumerableMethods/Repeat.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sandbox.Api.Controllers.Linq.EnumerableMethods
+{
+    public class Repeat : ControllerBase
+    {
+
+        public IActionResult BasicExample()
+        {
+            IEnumerable<string> strings =
+                Enumerable.Repeat("I like programming.", 15);
+
+            foreach (var str in strings)
+            {
+                Console.WriteLine(str);
+            }
+            return Ok();
+        }
+    }
+}
