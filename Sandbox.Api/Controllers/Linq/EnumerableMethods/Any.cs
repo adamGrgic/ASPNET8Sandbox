68793179de332ac386fdad4e3fdaed9c72a4dcd4
adamGrgic <argrgic@gmail.com>
2024-08-25 18:30:36 -0600

    Enumerable.Any

---
+++ Sandbox.Api/Controllers/Linq/EnumerableMethods/Any.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sandbox.Api.Controllers.Linq.EnumerableMethods
+{
+    public class Any : Controller
+    {
+        public IActionResult BasicExample()
+        {
+            List<int> numbers = new List<int> { 1, 2 };
+            bool hasElements = numbers.Any();
+
+            Console.WriteLine("The list {0} empty.",
+                hasElements ? "is not" : "is");
+            return Ok();
+        }
+    }
+}
