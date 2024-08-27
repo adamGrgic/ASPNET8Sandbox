868c744016fef136d05f7de847e7a824c8305370
adamGrgic <argrgic@gmail.com>
2024-08-26 22:12:21 -0600

    Enumerable.Contains

---
+++ Sandbox.Api/Controllers/Linq/EnumerableMethods/Contains.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sandbox.Api.Controllers.Linq.EnumerableMethods
+{
+    public class Contains : Controller
+    {
+        public Contains()
+        {
+            
+        }
+
+        public IActionResult BasicExample()
+        {
+            List<string> fruits =
+            new List<string> { "apple", "passionfruit", "banana", "mango",
+                    "orange", "blueberry", "grape", "strawberry" };
+
+            IEnumerable<string> query = fruits.Where(fruit => fruit.Length < 6);
+
+            foreach (string fruit in query)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            return Ok();
+        }
+    }
+}
