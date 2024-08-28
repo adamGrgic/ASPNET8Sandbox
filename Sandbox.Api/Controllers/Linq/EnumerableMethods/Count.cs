528d4d105b45d1df7ce64de4c8835b61cca4778f
adamGrgic <argrgic@gmail.com>
2024-08-27 23:30:50 -0600

    Enumerable.Count

---
+++ Sandbox.Api/Controllers/Linq/EnumerableMethods/Count.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sandbox.Api.Controllers.Linq.EnumerableMethods
+{
+    public class Count : Controller
+    {
+        public Count()
+        {
+
+        }
+
+        public IActionResult BasicExample()
+        {
+            string[] fruits = { "apple", "banana", "mango", "orange", "passionfruit", "grape" };
+
+            try
+            {
+                int numberOfFruits = fruits.Count();
+                Console.WriteLine(
+                    "There are {0} fruits in the collection.",
+                    numberOfFruits);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The count is too large to store as an Int32.");
+                Console.WriteLine("Try using the LongCount() method instead.");
+            }
+        }
+    }
+}
