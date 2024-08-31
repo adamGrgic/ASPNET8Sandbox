2c8e4225c8402c5e92462b970decccfd94758793
adamGrgic <argrgic@gmail.com>
2024-08-30 18:06:15 -0600

    Enumerable.DefaultIfEmpty

---
+++ Sandbox.Api/Modules/Linq/EnumerableMethods/DefaultIfEmpty.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sandbox.Api.Controllers.Linq.EnumerableMethods;
+
+namespace Sandbox.Api.Modules.Linq.EnumerableMethods
+{
+    public class DefaultIfEmpty : Controller
+    {
+        public static void DefaultIfEmptyEx1()
+        {
+            List<Pet> pets =
+                new List<Pet>{ new Pet { Name="Barley", Age=8 },
+                       new Pet { Name="Boots", Age=4 },
+                       new Pet { Name="Whiskers", Age=1 } };
+
+            foreach (Pet pet in pets.DefaultIfEmpty())
+            {
+                Console.WriteLine(pet.Name);
+            }
+        }
+
+    }
+}
