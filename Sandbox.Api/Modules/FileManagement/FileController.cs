77391efafcd1a3939f5c6bd76c1bed4d713ad316
adamGrgic <argrgic@gmail.com>
2024-09-14 15:14:11 -0600

    file utility and module

---
+++ Sandbox.Api/Modules/FileManagement/FileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sandbox.Api.SandboxUtils;
+
+namespace Sandbox.Api.Modules.FileManagement
+{
+    [Route("[controller]/[action]")]
+    public class FileController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult CreateTextFile(string name)
+        {
+
+            FileUtility.CreateTextFile(name);
+
+            return Ok();
+        }
+    }
+}
--- Sandbox.Api/SandboxUtils/FileUtility.cs
+++ Sandbox.Api/SandboxUtils/FileUtility.cs
@@ -5,7 +5,17 @@
         public static void CreateTextFile(string name)
         {
             var currentDirectory = Directory.GetCurrentDirectory();
-            File.CreateText($"{currentDirectory}/tmp/{name}.txt");
+            var path = $"{currentDirectory}/tmp/{name}.txt";
+
+            if (!File.Exists(path))
+            {
+                using (var streamWriter = File.CreateText(path))
+                {
+                    streamWriter.WriteLine("Hello");
+                    streamWriter.WriteLine("And");
+                    streamWriter.WriteLine("Welcome");
+                }
+            }
         }
     }
 }
