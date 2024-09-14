77113badfd9335933882132a04e3832996bdbd96
adamGrgic <argrgic@gmail.com>
2024-09-14 11:07:51 -0600

    react sandbox init init

---
+++ sandbox.reactclient/src/App.tsx
@@ -0,0 +1,55 @@
+import './App.css';
+
+//interface Forecast {
+//    date: string;
+//    temperatureC: number;
+//    temperatureF: number;
+//    summary: string;
+//}
+
+function App() {
+    // sample content
+    //const [forecasts, setForecasts] = useState<Forecast[]>();
+
+    //useEffect(() => {
+    //    populateWeatherData();
+    //}, []);
+
+    //const contents = forecasts === undefined
+    //    ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
+    //    : <table className="table table-striped" aria-labelledby="tabelLabel">
+    //        <thead>
+    //            <tr>
+    //                <th>Date</th>
+    //                <th>Temp. (C)</th>
+    //                <th>Temp. (F)</th>
+    //                <th>Summary</th>
+    //            </tr>
+    //        </thead>
+    //        <tbody>
+    //            {forecasts.map(forecast =>
+    //                <tr key={forecast.date}>
+    //                    <td>{forecast.date}</td>
+    //                    <td>{forecast.temperatureC}</td>
+    //                    <td>{forecast.temperatureF}</td>
+    //                    <td>{forecast.summary}</td>
+    //                </tr>
+    //            )}
+    //        </tbody>
+    //    </table>;
+
+    return (
+        <div>
+            <h1>Sandbox React Application</h1>
+        </div>
+    );
+
+    // samplke function
+    //async function populateWeatherData() {
+    //    const response = await fetch('weatherforecast');
+    //    const data = await response.json();
+    //    setForecasts(data);
+    //}
+}
+
+export default App;
